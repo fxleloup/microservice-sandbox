b7f7cc7c8d6522c4204924ab05ff330b00f2c785
Stéphane Krattinger <stef.krattinger@gmail.com>
2018-04-21 19:57:28 +0200

    fix publishing artifact

--- .teamcity/MicroserviceSandbox/buildTypes/MicroserviceSandbox_Build.kt
+++ .teamcity/MicroserviceSandbox/buildTypes/MicroserviceSandbox_Build.kt
@@ -10,7 +10,7 @@ object MicroserviceSandbox_Build : BuildType({
     uuid = "86a5392d-63ce-4be0-9f16-f32f89b04336"
     id = "MicroserviceSandbox_Build"
     name = "Build"
-    artifactRules = "+:**/case/case-infra/target/pitreports/** => pitreports"
+    artifactRules = "+:**/case/case-infra/target/pitreports => pitreports"
 
     vcs {
         root(MicroserviceSandbox.vcsRoots.MicroserviceSandbox_HttpsGithubComMcKrattMicroserviceSandboxRefsHeadsMaster)
