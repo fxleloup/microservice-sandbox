c37637481a8ef27c9bea39454a1d298bc3480ee9
Stéphane Krattinger <stef.krattinger@gmail.com>
2018-06-09 09:29:50 +0200

    update local script

--- case/case-application/scripts/localpgdb.sh
+++ case/case-application/scripts/localpgdb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-container_name='casepgdb'
+container_name='case-db'
 if [ "$(docker ps -aq -f name=${container_name})" ];
     then docker start ${container_name}
-    else docker run -d --name ${container_name} -p 27123:5432 -e POSTGRES_PASSWORD='123456' -e POSTGRES_USER=caseDb_user -e POSTGRES_DB=Case postgres:9.6.8
+    else docker run -d --name ${container_name} -p 5432:5432 -e POSTGRES_PASSWORD='123456' -e POSTGRES_USER=case_user -e POSTGRES_DB=case-user postgres:9.6.8-alpine
 fi
