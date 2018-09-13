0a7e8efc0e12ad49e97558a5c75d20c75fb1c43b
Stéphane Krattinger <stef.krattinger@gmail.com>
2018-09-13 10:27:54 +0200

    add new docker image

--- travis-build.sh
+++ travis-build.sh
@@ -3,4 +3,7 @@ mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     cd business-number
     mvn dockerfile:build@build dockerfile:tag@latest dockerfile:tag@version dockerfile:push@latest dockerfile:push@version
+
+    cd ../case/case-application/
+    mvn dockerfile:build@build dockerfile:tag@latest dockerfile:tag@version dockerfile:push@latest dockerfile:push@version
 fi
