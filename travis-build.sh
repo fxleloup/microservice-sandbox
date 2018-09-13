2a3b5c4e7d69ea257571797afb880629364fc27c
Stéphane Krattinger <stef.krattinger@gmail.com>
2018-09-13 18:32:30 +0200

    third try

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
-if [[ $TRAVIS_BRANCH == 'master' ]]; then
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
     cd business-number
     mvn dockerfile:build@build dockerfile:tag@latest dockerfile:tag@version dockerfile:push@latest dockerfile:push@version
 
