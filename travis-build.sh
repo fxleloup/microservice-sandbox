e8092d7d4b8af2a910c8d1926fafdab00dae0e1e
Stéphane Krattinger <stef.krattinger@gmail.com>
2018-09-13 17:58:31 +0200

    another try to have docker only on master

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
     cd business-number
     mvn dockerfile:build@build dockerfile:tag@latest dockerfile:tag@version dockerfile:push@latest dockerfile:push@version
 
