657a506edbb7b9e6c7a9873ed0b6cdb110f3f6e0
Stéphane Krattinger <stef.krattinger@gmail.com>
2018-09-13 10:17:12 +0200

    refactoring build travis and docker build

---
+++ travis-build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    cd business-number
+    mvn dockerfile:build@build dockerfile:tag@latest dockerfile:tag@version dockerfile:push@latest dockerfile:push@version
+fi
